a29534c8a45292d6f5030b594dcc49723b85ec41
Hidde Jense <JenseH@cardiff.ac.uk>
2026-08-07 10:40:45 +0100

    Updated example.

--- example.py
+++ example.py
@@ -116,83 +116,115 @@ deriv_params = [
 
 params_values = fg_params | sys_params
 
-free_params = like.parameters + fg_model.parameters
+model_params = like.parameters + fg_model.parameters
 
 priors = {
     "calG_all": lambda x: jsc.stats.norm.logpdf(x, loc=1.0, scale=0.003),
 
-    "cal_dr6_pa4_f220": lambda x: jsc.stats.norm.logpdf(x, loc=1.0, scale=0.013),
-    "cal_dr6_pa5_f090": lambda x: jsc.stats.norm.logpdf(x, loc=1.0, scale=0.0016),
-    "cal_dr6_pa5_f150": lambda x: jsc.stats.norm.logpdf(x, loc=1.0, scale=0.0020),
-    "cal_dr6_pa6_f090": lambda x: jsc.stats.norm.logpdf(x, loc=1.0, scale=0.0018),
-    "cal_dr6_pa6_f150": lambda x: jsc.stats.norm.logpdf(x, loc=1.0, scale=0.0024),
+    "cal_dr6_pa4_f220": lambda x: jsc.stats.norm.logpdf(x, loc=1.0,
+                                                        scale=0.013),
+    "cal_dr6_pa5_f090": lambda x: jsc.stats.norm.logpdf(x, loc=1.0,
+                                                        scale=0.0016),
+    "cal_dr6_pa5_f150": lambda x: jsc.stats.norm.logpdf(x, loc=1.0,
+                                                        scale=0.0020),
+    "cal_dr6_pa6_f090": lambda x: jsc.stats.norm.logpdf(x, loc=1.0,
+                                                        scale=0.0018),
+    "cal_dr6_pa6_f150": lambda x: jsc.stats.norm.logpdf(x, loc=1.0,
+                                                        scale=0.0024),
 
-    "bandint_shift_dr6_pa4_f220": lambda x: jsc.stats.norm.logpdf(x, loc=0.0, scale=3.6),
-    "bandint_shift_dr6_pa5_f090": lambda x: jsc.stats.norm.logpdf(x, loc=0.0, scale=1.0),
-    "bandint_shift_dr6_pa5_f150": lambda x: jsc.stats.norm.logpdf(x, loc=0.0, scale=1.3),
-    "bandint_shift_dr6_pa6_f090": lambda x: jsc.stats.norm.logpdf(x, loc=0.0, scale=1.2),
-    "bandint_shift_dr6_pa6_f150": lambda x: jsc.stats.norm.logpdf(x, loc=0.0, scale=1.1),
+    "bandint_shift_dr6_pa4_f220": lambda x: jsc.stats.norm.logpdf(x, loc=0.0,
+                                                                  scale=3.6),
+    "bandint_shift_dr6_pa5_f090": lambda x: jsc.stats.norm.logpdf(x, loc=0.0,
+                                                                  scale=1.0),
+    "bandint_shift_dr6_pa5_f150": lambda x: jsc.stats.norm.logpdf(x, loc=0.0,
+                                                                  scale=1.3),
+    "bandint_shift_dr6_pa6_f090": lambda x: jsc.stats.norm.logpdf(x, loc=0.0,
+                                                                  scale=1.2),
+    "bandint_shift_dr6_pa6_f150": lambda x: jsc.stats.norm.logpdf(x, loc=0.0,
+                                                                  scale=1.1),
 
     "a_gtt": lambda x: jsc.stats.norm.logpdf(x, loc=7.95, scale=0.32),
     "a_gte": lambda x: jsc.stats.norm.logpdf(x, loc=0.432, scale=0.03),
     "a_gee": lambda x: jsc.stats.norm.logpdf(x, loc=0.1681, scale=0.017),
 }
 
-""" JAX likes working with vectors rather than dictionaries, so we are going to
-    turn our dictionary into a vector (and then turn it back later, don't
-    think too hard about it). """
+""" Define our log-likelihood and log-prior functions. """
 
-theta = jnp.array([ params_values[k] for k in free_params ])
-
-fixed_params = {
-    k : params_values[k] for k in params_values if k not in free_params
-}
-
-""" Define our log-likelihood and log-prior functions. Note that these are
-    *negative* log(X), because some standard implementations are searching for
-    function minima rather than maxima. """
 
 @jax.jit
 def logprior(theta):
-    params = { k: theta[i] for i, k in enumerate(free_params) } | fixed_params
+    # Note that this function is not normalized.
+    params = {k: theta[i] for i, k in enumerate(model_params)}
     logp = 0.0
     for k in priors:
         logp = logp + priors[k](params[k])
-    return -logp
+    return logp
+
 
 @jax.jit
 def loglike(theta):
     theta_like = theta[:len(like.parameters)]
     theta_fg = theta[len(like.parameters):]
     foregrounds = fg_model.get_foreground_model(theta_fg)
-    logl = -0.5 * like.chisquare(spec[:,0], spec[:,3], spec[:,1], foregrounds, theta_like)
-    return -logl
+    logl = -0.5 * like.chisquare(spec[:, 0], spec[:, 3], spec[:, 1],
+                                 foregrounds, theta_like)
+    return logl
+
 
 @jax.jit
 def logpost(theta):
     return logprior(theta) + loglike(theta)
 
 
+# These are the parameters with respect to which we want to differentiate.
+parameters_baseline = [
+    "a_tSZ", "a_kSZ", "alpha_tSZ", "xi",
+    "a_c", "beta_c", "a_p", "beta_p", "a_s", "beta_s",
+    "a_gtt", "a_gte", "a_gee", "a_pste", "a_psee",
+    "calG_all",
+    "cal_dr6_pa4_f220", "cal_dr6_pa5_f090", "cal_dr6_pa5_f150",
+    "cal_dr6_pa6_f090", "cal_dr6_pa6_f150",
+    "calE_dr6_pa5_f090", "calE_dr6_pa5_f150",
+    "calE_dr6_pa6_f090", "calE_dr6_pa6_f150",
+    "bandint_shift_dr6_pa4_f220", "bandint_shift_dr6_pa5_f090",
+    "bandint_shift_dr6_pa5_f150", "bandint_shift_dr6_pa6_f090",
+    "bandint_shift_dr6_pa6_f150",
+]
+
+# We need a log-posterior function that only takes in baseline parameters and
+# keeps the remainder fixed. We create a default list of parameters, and then
+# keep track of the indices of the free parameters.
+fit_idx = jnp.array([model_params.index(p) for p in parameters_baseline])
+theta_default = jnp.array([params_values[p] for p in model_params])
+
+
+@jax.jit
+def logpost_baseline(theta_x):
+    # Note that in numpy we could write `theta[fit_idx] = theta_x`
+    # But in jax, arrays are immutable, so we need to use this `.at[i].set(x)`.
+    theta = theta_default.at[fit_idx].set(theta_x)
+    return logpost(theta)
+
+
+theta_baseline = jnp.array([params_values[p] for p in parameters_baseline])
+
 """ We can simply evaluate these functions directly: """
-print("log(prior) = ", logprior(theta))
-print("log(like)  = ", loglike(theta))
-print("log(post)  = ", logpost(theta))
+print("log(prior) = ", logprior(theta_default))
+print("log(like)  = ", loglike(theta_default))
+print("log(post)  = ", logpost(theta_default))
 
 """ Or we can do some jax things with them: """
-grad = jax.grad(logpost)
-hess = jax.hessian(logpost)
+grad = jax.grad(logpost_baseline)
+hess = jax.hessian(logpost_baseline)
 
 # These two are functions, we can evaluate them at the same values and find
 # derivatives:
-H = hess(theta)
+H = hess(theta_baseline)
 
-# We need to take out only the entries of the Hessian that we care about.
-idx = jnp.array([ free_params.index(p) for p in deriv_params ])
-H = H[:,idx][idx,:]
-
-cov = jnp.linalg.inv(H)
+# Fisher estimate of the error.
+cov = jnp.linalg.inv(-H)
 err = np.sqrt(np.diag(cov))
 
 for i, par in enumerate(deriv_params):
-    print(f"{par:>30s} = {params_values[par]:8.2f} +/- " + (f"{err[i]:4.2e}" if err[i] < 0.01 else f"{err[i]:4.2f}"))
-
+    print(f"{par:>30s} = {params_values[par]:8.2f} +/- " +
+          (f"{err[i]:4.2e}" if err[i] < 0.01 else f"{err[i]:4.2f}"))
