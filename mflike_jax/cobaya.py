b34ac40668d7885466ec6efc86c0f1940047e70f
Hidde Jense <JenseH@cardiff.ac.uk>
2026-09-22 21:24:54 +0100

    Added lensing corrections to notebook.

--- mflike_jax/cobaya.py
+++ mflike_jax/cobaya.py
@@ -10,8 +10,8 @@ from typing import Optional
 
 
 class MFLike_jax_cobaya(Likelihood):
-    like_config_file: str | dict
-    fg_config_file: str | dict
+    like_config_file: Optional[str | dict] = None
+    fg_config_file: Optional[str | dict] = None
 
     def initialize(self):
         self.like = like.MFLike_jax(self.like_config_file)
@@ -38,15 +38,14 @@ class MFLike_jax_cobaya(Likelihood):
 
 
 class Lensing_jax_cobaya(Likelihood):
-    config_file: str | dict
+    config_file: Optional[str | dict] = None
     corr_config_file: Optional[str | dict] = None
 
     def initialize(self):
         self.like = lensing.Lensing_jax(self.config_file)
-        if self.corr_config_file is not None:
-            self.theory = lensing_corrections.LensingCorrections(
-                self.corr_config_file
-            )
+        self.theory = lensing_corrections.LensingCorrections(
+            self.corr_config_file
+        )
 
     def get_requirements(self):
         reqs = {}
@@ -58,11 +57,16 @@ class Lensing_jax_cobaya(Likelihood):
 
     def logp(self, **params):
         cl = self.provider.get_Cl(ell_factor=True)
+<<<<<<< HEAD
 
         corr = None
         if self.theory is not None:
             theta_th = np.array([params[k] for k in self.theory.parameters])
             corr = self.theory.get_corrections(theta_th)
+=======
+        theta_th = np.array([params[k] for k in self.theory.parameters])
+        corr = self.theory.get_corrections(theta_th)
+>>>>>>> parent of 64211d2 (Made lensing corrections optional.)
 
         theta_like = np.array([params[k] for k in self.like.parameters])
         chi2 = self.like.chisquare(cl["pp"], corr, theta_like)
